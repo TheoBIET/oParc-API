8d19996043c3b50747380e161cf20649282e5d1f
Théo BIET <dev.theobiet@gmail.com>
2021-07-27 14:27:27 +0200

    FT | Add express-session

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const session = require('express-session');
 const PORT = process.env.PORT || 3003;
 const {
     dashboardRouter,
     apiRouter
 } = require('./app/routers');
+
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }
+}));
 
 app.use('/api', apiRouter);
 app.use('/admin', dashboardRouter);
