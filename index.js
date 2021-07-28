435ea9bbc3c99dd2568f52abfd88ddc30ddce514
Théo BIET <dev.theobiet@gmail.com>
2021-07-28 17:05:56 +0200

    1st dashboard version

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ const app = express();
 const session = require("express-session");
 const PORT = process.env.PORT || 3003;
 const { dashboardRouter, apiRouter } = require("./app/routers");
+const cors = require("cors");
+
+const corsOptions = {
+    origin: "http://localhost:3001",
+    credentials: true,
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.urlencoded({ extended: false }));
 
@@ -16,6 +24,7 @@ app.use(
     })
 );
 
+app.use(express.static("public"));
 app.use("/api", apiRouter);
 app.use("/admin", dashboardRouter);
 
